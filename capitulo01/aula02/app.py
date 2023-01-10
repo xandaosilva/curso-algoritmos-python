d073530a01215634b7fe1c117764f61c229c19ce
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-01-10 08:16:08 +0044

    Exercícios em python

---
+++ capitulo01/aula02/app.py
@@ -0,0 +1,9 @@
+name = "Alexandre Rogério"
+expression_a = 5 * 3 + 10
+expression_b = 10 + 2 / 3
+expression_c = 12 + 4 - ( 10 / 2 )
+
+print(name)
+print(expression_a)
+print(expression_b)
+print(expression_c)
