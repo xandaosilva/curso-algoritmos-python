a03de5f272336453659b7f7cba505d65cf41af74
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-01-10 09:42:45 +0044

    Variáveis em python

---
+++ capitulo02/aula01/app.py
@@ -0,0 +1,6 @@
+name = "Alexandre"
+age = 29
+text = "Lorem ipsum dolor sit amet"
+
+print(name, type(name), age, type(age))
+print(text)
---
+++ capitulo02/aula02/app.py
@@ -0,0 +1,13 @@
+full_name = "Alexandre Rogério"
+age = 29
+height = 1.70
+weight = 110.5
+year2023 = 2023
+
+value_x = 10
+value_y = 12
+sum_x_y = value_x + value_y
+
+print(full_name, age, height, weight)
+print(year2023)
+print(sum_x_y)
