9f72dfed2967e10f2340906407b4094a8d02ccb2
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-01-12 07:06:11 +0044

    Valores booleanos

--- capitulo02/aula02/app.py
+++ capitulo02/aula02/app.py
@@ -3,6 +3,7 @@ age = 29
 height = 1.70
 weight = 110.5
 year2023 = 2023
+cnh = age >= 18
 
 value_x = 10
 value_y = 12
@@ -11,3 +12,4 @@ sum_x_y = value_x + value_y
 print(full_name, age, height, weight)
 print(year2023)
 print(sum_x_y)
+print(cnh)
